db5a6e6c6fae844c0650ea1c8ced987b5990491a
André Teixeira <andre-x8@hotmail.com>
2025-04-23 22:47:33 -0300

    refatoracao dos cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { BalanceCardComponent } from './components/balance-card/balance-card.component';
 import { CreditCardComponent } from './components/credit-card/credit-card.component';
 import { DebitCardComponent } from './components/debit-card/debit-card.component';
+import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BalanceCardComponent,
     CreditCardComponent,
-    DebitCardComponent
+    DebitCardComponent,
+    ClientInfosCardComponent,
+    CardItemComponent
   ],
   imports: [
     BrowserModule,
